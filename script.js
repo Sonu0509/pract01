baf8bdc1cb853f8ea0cbd3ba2be17180fb5295a4
Sonu <katiyaaramit97@gmail.com>
2025-07-18 18:53:09 +0530

    thekatiyars

---
+++ script.js
@@ -0,0 +1,16 @@
+// function sendMessage(event) {
+//   event.preventDefault();
+  
+//   const name = document.getElementById("name").value;
+//   alert("Thank you, " + name + "! Your message has been sent.");
+  
+//   // Clear the form
+//   document.getElementById("name").value = "";
+//   document.getElementById("email").value = "";
+//   document.getElementById("message").value = "";
+// }
+  
+
+ function sonu (){
+ document.getElementById("user").innerHTML="hello! ji"
+}
