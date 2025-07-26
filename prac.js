7b97c182848f9ed5a4f967586692633f55f245a2
Sonu <katiyaaramit97@gmail.com>
2025-07-26 19:03:40 +0530

    parct

---
+++ prac.js
@@ -0,0 +1,9 @@
+
+let btn =document.createElement("button");
+btn.innerText="click me";
+btn.style.backgroundColor ="green";
+btn.style.color="white"
+
+
+
+document.getElementById("btn").appendChild(btn);
