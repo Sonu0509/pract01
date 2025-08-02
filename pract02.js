ea2cc358654476f59cbef5f8a68293ebf6528bbc
Sonu <katiyaaramit97@gmail.com>
2025-08-02 18:30:53 +0530

    obj

---
+++ pract02.js
@@ -0,0 +1,25 @@
+// let a =["a","b","c","d", ]
+// let b =["q","w","e","s"]
+
+// let c=a.concat(b);
+// console.log(c)
+// let c =[...a ,...b]
+
+// console.log(c)
+
+
+
+//  let a =["a","b","c","d", ]
+//  let w =a.join();
+//  console.log(w)
+//  let e =w.split(",");
+//  console.log(e)
+//  let r = e.split(",");
+
+
+
+let str1 = ["a","b","c","d","e"];
+str1.forEach((item)=>{
+      let a= [item];
+      console.log(a);
+});
